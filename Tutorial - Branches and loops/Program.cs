30ae45c3a2d72bc35f039f0d2e51f1e150c7651b
Marta Debowska <debowska.marta@outlook.com>
2023-08-29 22:02:18 +0200

    Renamed Project and added new Program for Branches and loops

---
+++ Tutorial - Branches and loops/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Tutorial___Branches_and_loops
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            
+        {
+            //Testing if statement from examples
+
+            int a = 3;
+            int b = 2;
+            if (a + b > 10)
+                  Console.WriteLine("The answer is greater than 10.");
+            else
+                  Console.WriteLine("The answer is not greater than 10");
+
+            
+
+            int c = 7;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+
+            //Testing loops to repeat operations
+
+            int counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+        }
+    }
+} 
