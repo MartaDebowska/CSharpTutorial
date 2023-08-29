23a9225d1fbc3881e25365380eb850f6dec2dd2a
Marta Debowska <debowska.marta@outlook.com>
2023-08-29 21:08:51 +0200

    Added additional numbers operations tests

--- Tutorial 1 - Numbers/Program.cs
+++ Tutorial 1 - Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Formats.Asn1;
+
 Console.WriteLine("Hello, World!");
 
 
@@ -21,20 +23,54 @@ Console.WriteLine(c);
 c = a / b;
 Console.WriteLine(c);
 
-var names = new List<string> { "<name>", "Ana", "Felipe" };
+var names = new List<string> { "Marta", "Ana", "Felipe" };
 foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
 }
 
-var fibonacciNumbers = new List<int> { 1, 1 };
 
-var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+// WorkWithIntegers();
+OrderPrecedence();
 
-fibonacciNumbers.Add(previous + previous2);
-
-foreach (var item in fibonacciNumbers)
+void OrderPrecedence()
 {
-    Console.WriteLine(item);
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
 }
+
+int aa = 7;
+int bb = 4;
+int cc = 3;
+int dd = (aa + bb) / cc;
+int ee = (aa + bb) % cc;
+Console.WriteLine($"quotient: {dd}");
+Console.WriteLine($"remainder: {ee}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
+
+// Challange to calculate the area of a circle
+
+double radius = 2.50;
+double area = radius * radius * Math.PI;
+Console.WriteLine($"Area of the circle equals to {area}");
