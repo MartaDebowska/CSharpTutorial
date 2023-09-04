f04b7183ca5a8d689e54ad339c99f0eb35327ad4
Marta Debowska <debowska.marta@outlook.com>
2023-09-04 21:33:56 +0200

    Modified file for Module 1

--- Training - Module 1/Program.cs
+++ Training - Module 1/Program.cs
@@ -8,6 +8,18 @@
             Console.Write("Congratulations!");
             Console.Write(" ");
             Console.Write("You wrote your first line of code.");
+
+            System.Console.WriteLine("Hello World!");
+
+            Console.WriteLine('b');
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(123);
+            Console.WriteLine(0.25F);
+            Console.WriteLine(2.625);
+            Console.WriteLine(12.39816m);
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
         }
     }
 }
