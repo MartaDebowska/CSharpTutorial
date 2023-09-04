b2d376c3a809d2ba09c877e15662b733bb98f175
Marta Debowska <debowska.marta@outlook.com>
2023-09-04 21:04:08 +0200

    Added new console project to do training modules

---
+++ Training - Module 1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Training___Module_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.Write("You wrote your first line of code.");
+        }
+    }
+}
